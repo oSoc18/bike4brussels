3c885c7dbdd7a0de806397486f3ac933cb922aae
Fizz55 <neyt.kr@gmail.com>
2018-07-10 23:41:19 +0200

    Transition animation smoothed, sidebar close button right-aligned, fixed sidebar scrolling

--- js/uihelper.js
+++ js/uihelper.js
@@ -1,7 +1,7 @@
 var isSidebarVisible = false;
 
 function toggleSidebar() {
-    $("#sidebar-right-container").animate({width: 'toggle'}, 350);
+    $("#sidebar-right-container").toggleClass('hidden-sidebar');
     isSidebarVisible = !isSidebarVisible;
     if (isSidebarVisible) {
         $("#top-overlay-center-spacer").addClass("col-lg-1").addClass("col-md-0").removeClass("col-lg-2").removeClass("col-md-2");
@@ -10,7 +10,6 @@ function toggleSidebar() {
         $("#top-overlay-center-spacer").addClass("col-lg-2").addClass("col-md-2").removeClass("col-lg-1").removeClass("col-md-0");
         $("#top-overlay-input").addClass("col-lg-8").removeClass("col-lg-7");
     }
-
 }
 
 
