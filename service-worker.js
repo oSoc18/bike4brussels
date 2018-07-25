314a46fd071ab1a52468a562ddf2dd4fc95d2c6a
kneyt <neyt.kr@gmail.com>
2018-07-25 16:15:20 +0200

    Don't cache cross-origin (doesn't work)

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@
 // in turn trigger the install event again.
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
-const TILE_SERVER = 'https://a.tiles.mapbox.com/';
+//const TILE_SERVER = 'https://a.tiles.mapbox.com/';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
@@ -57,7 +57,7 @@ self.addEventListener('activate', event => {
 // from the network before returning it to the page.
 self.addEventListener('fetch', event => {
     // Skip cross-origin requests, like those for Google Analytics.
-    if (event.request.url.startsWith(self.location.origin) || event.request.url.startsWith(TILE_SERVER)) {
+    if (event.request.url.startsWith(self.location.origin)){//} || event.request.url.startsWith(TILE_SERVER)) {
         event.respondWith(
             caches.match(event.request).then(cachedResponse => {
                 if (cachedResponse) {
