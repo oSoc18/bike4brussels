48d3d1a6414010a158bdb7934103da25b6eb66bc
kneyt <neyt.kr@gmail.com>
2018-07-25 15:49:35 +0200

    Small fix add to homescreen

--- js/uihelper.js
+++ js/uihelper.js
@@ -399,7 +399,9 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
     // Update UI notify the user they can add to home screen
-    $("#btnAddToHomescreen").show();
+    if (deferredPrompt) {
+        $("#btnAddToHomescreen").show();
+    }
 });
 
 document.getElementById("btnAddToHomescreen").addEventListener('click', (e) => {
