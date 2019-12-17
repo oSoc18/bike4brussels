fa306d845ba41df0a95649636fb28ce2c5425741
Jonathan Beliën <dev@jbelien.be>
2019-12-17 20:42:01 +0100

    Update constants.js
    Comment unused parameters in "urls" variable.

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,8 @@
 const mapboxAccessCode = "pk.eyJ1IjoibWljaGllbC1sZXltYW4iLCJhIjoiY2p6bDY2OTdjMGtpbjNic2FycHNqYzhrMiJ9.ixmQT1F7yzxXksnHbBco0A"; // OKBE token
 
 const urls = {
-    mapStyle: 'https://openmaptiles.github.io/positron-gl-style/style-cdn.json',
-    network: 'https://cyclenetworks.osm.be/brumob/data/network.geojson',
+    // mapStyle: 'https://openmaptiles.github.io/positron-gl-style/style-cdn.json',
+    // network: 'https://cyclenetworks.osm.be/brumob/data/network.geojson',
     route: 'https://bike4brussels.osm.be/api',
     geocoder: `https://api.mapbox.com/geocoding/v5/mapbox.places/{0}.json?access_token=${mapboxAccessCode}&proximity=50.861%2C4.356&country=BE&bbox=3.9784240723%2C50.6485897217%2C4.7282409668%2C51.0552073386&limit=5&types=place,locality,neighborhood,address,poi`,
     reverseGeocoder: `https://api.mapbox.com/geocoding/v5/mapbox.places/{0},{1}.json?limit=1&access_token=${mapboxAccessCode}`
